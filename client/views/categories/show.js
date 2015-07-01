b2286add56446142f88fb7913cd738baf0aae64f
Nicolás López <nicolaslopezj@me.com>
2015-07-01 11:51:01 -0300

    order by

--- client/views/categories/show.js
+++ client/views/categories/show.js
@@ -1,3 +1,21 @@
+var getSort = function() {
+  if (Session.get('currentOrder') == 'populares') {
+    return { price: 1 };
+  }
+  if (Session.get('currentOrder') == 'menor-a-mayor-precio') {
+    return { price: 1 };
+  }
+  if (Session.get('currentOrder') == 'mayor-a-menor-precio') {
+    return { price: -1 };
+  }
+  if (Session.get('currentOrder') == 'nuevos') {
+    return { createdAt: -1 };
+  }
+  if (Session.get('currentOrder') == 'antiguos') {
+    return { createdAt: 1 };
+  }
+}
+
 Template.categoriesShow.onCreated(function () {
   var self = this;
   self.autorun(function () {
@@ -13,6 +31,11 @@ Template.categoriesShow.onRendered(function() {
   var self = this;
   $('.parallax').parallax();
   $('.dropdown-button').dropdown({ constrain_width: false });
+  $('select').material_select();
+
+  if (!Session.get('currentOrder')) {
+    Session.set('currentOrder', 'populares');
+  }
 
   self.autorun(function() {
     if (!Router.current().params.type) {
@@ -27,7 +50,7 @@ Template.categoriesShow.onRendered(function() {
     var container = document.querySelector('.masonry');
     var msnry = new Masonry(container, { itemSelector: '.col' });
     var id = Router.current().params.type || '';
-    Products.find({ category: id }).count();
+    Products.find({ category: id }, { sort: getSort() }).count();
     Tracker.afterFlush(function () {
       $('.dropdown-button').dropdown({ constrain_width: false });
       var msnry = new Masonry(container, { itemSelector: '.col' });
@@ -64,11 +87,14 @@ Template.categoriesShow.helpers({
     var item = _.findWhere(topCategories, { value: Router.current().params.value });
     return Categories.find({ category: item.label });
   },
-  products: function() {
+  productsWithPrice: function() {
     var id = Router.current().params.type || '';
-    var withPrice = Products.find({ category: id, price: { $ne: null }}, { sort: { price: 1 } }).fetch()
-    var withoutPrice = Products.find({ category: id, price: null }, { sort: { price: 1 } }).fetch()
-    return _.union(withPrice, withoutPrice);
+    var products = Products.find({ category: id, price: { $ne: null }}, { sort: getSort() }).fetch();
+    return Products.find({ category: id, price: { $ne: null }}, { sort: getSort() }).fetch();
+  },
+  productsWithoutPrice: function() {
+    var id = Router.current().params.type || '';
+    return Products.find({ category: id, price: null }, { sort: getSort() }).fetch();
   },
   getIdForSubcategory: function() {
     return String(this).toLowerCase().replace(/\s+/g, '');
@@ -79,5 +105,7 @@ Template.categoriesShow.helpers({
 });
 
 Template.categoriesShow.events({
-
+  'change .order-select': function(event, template) {
+    Session.set('currentOrder', $('select.order-select').val());
+  }
 });
