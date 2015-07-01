fc8dc9d33b5546e7d6885e7b4fa35ed58a26391f
Nicolás López <nicolaslopezj@me.com>
2015-07-01 12:02:10 -0300

    added infantil

--- orion/dictionary/images.js
+++ orion/dictionary/images.js
@@ -1,46 +1,53 @@
-orion.dictionary.addDefinition('muebles', 'images', 
+orion.dictionary.addDefinition('muebles', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría muebles',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('pisos', 'images', 
+orion.dictionary.addDefinition('pisos', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría pisos',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('decoracion', 'images', 
+orion.dictionary.addDefinition('decoracion', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría decoración',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('iluminacion', 'images', 
+orion.dictionary.addDefinition('iluminacion', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría iluminación',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('banos', 'images', 
+orion.dictionary.addDefinition('banos', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría baños',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('cocina', 'images', 
+orion.dictionary.addDefinition('cocina', 'images',
   orion.attribute('image', {
       label: 'Imagen categoría cocina',
       optional: true
   })
 );
 
-orion.dictionary.addDefinition('decoradores', 'images', 
+orion.dictionary.addDefinition('infantil', 'images',
+  orion.attribute('image', {
+      label: 'Imagen categoría infantil',
+      optional: true
+  })
+);
+
+orion.dictionary.addDefinition('decoradores', 'images',
   orion.attribute('image', {
       label: 'Imagen decoradores',
       optional: true
